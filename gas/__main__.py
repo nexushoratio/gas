5cecbd5ab942084f6ca4d05401118f6d3227b654
Mike Castle <dalgoda@gmail.com>
2025-11-17 15:59:48 -0800

    Initial project files.

---
+++ gas/__main__.py
@@ -0,0 +1,28 @@
+"""Manage Google Apps Script projects from the command line.
+
+Mostly a work-alike for Google's own "clasp", only in Python.
+"""
+
+import sys
+import typing
+
+from mundane import app
+from mundane import log_mgr
+
+
+def main() -> typing.NoReturn:
+    """The GAS app."""
+    log_mgr.set_root_log_level('INFO')
+    gas_app = app.ArgparseApp(
+        use_log_mgr=True, use_docstring_for_description=sys.modules[__name__]
+    )
+    modules = ()
+    gas_app.register_global_flags(modules)
+    gas_app.register_shared_flags(modules)
+    gas_app.register_commands(modules)
+
+    sys.exit(gas_app.run())
+
+
+if __name__ == '__main__':  # pragma: no cover
+    main()
